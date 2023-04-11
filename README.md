dfd77cccb738ae568b3ef95820d5ca4f48e96146
jfecunha <joao.o.cunha92@gmail.com>
2023-04-11 16:00:22 +0100

    feat: docker container for tesseract

--- README.md
+++ README.md
@@ -6,3 +6,13 @@
 ```sh
 docker build --rm --tag tesserarct -f .\docker\tesseract\Dockerfile .
 ```
+
+## Fine tuning LayoutLM on arquivo dataset
+
+https://colab.research.google.com/github/NielsRogge/Transformers-Tutorials/blob/master/LayoutLMv2/FUNSD/Fine_tuning_LayoutLMv2ForTokenClassification_on_FUNSD_using_HuggingFace_Trainer.ipynb#scrollTo=SedlLnHKZLAc
+
+## Docker copy
+
+```sh
+    docker cp test-tesesract:/tesseract/data .
+```
--- scripts/apply_tesseract.py
+++ scripts/apply_tesseract.py
@@ -1,8 +1,18 @@
 """Script that applies Tesseract OCR into news pictures."""
+import json
+
 from PIL import Image
-import os
+from pathlib import Path
 
 import pytesseract
 
+img = Image.open('cmjornal-20200901070349.png').convert("RGB")
+
 # Get verbose data including boxes, confidences, line and page numbers
-print(pytesseract.image_to_data(Image.open('cmjornal-20200901070349.png')))
+output = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
+
+source_file = Path('data/')
+source_file.mkdir(parents=True, exist_ok=True)
+
+with open(Path(source_file, 'result_dummy.json'), 'w') as file:
+    json.dump(output, file)
